9e92658e19eb83cc7a54062f0cae82471b1d0551
VanessaMetayani <79550906+VanessaMetayani@users.noreply.github.com>
2021-02-24 11:05:05 +0700

    Initial Commit

---
+++ src/com/vanessa/controller/DemoMainController.java
@@ -0,0 +1,27 @@
+package com.vanessa.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DemoMainController {
+
+    @FXML
+    private TextField txtInput;
+
+    @FXML
+    private Label lblOutput;
+
+    @FXML
+    private void sendQueryAction(ActionEvent actionEvent) {
+        if(!txtInput.getText().trim().isEmpty()){
+            lblOutput.setText((txtInput.getText().trim()));
+        }else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Please fill input");
+            alert.showAndWait();
+        }
+    }
+}
